111bb6420e8d548f2d8be32d20f083875aaf5195
tboquet <thomas.boquet@hec.ca>
2018-05-18 16:11:58 -0400

    + prints

--- code/model.py
+++ code/model.py
@@ -192,6 +192,9 @@ class INIT_STAGE_G(nn.Module):
         self.upsample4 = upBlock(ngf // 8, ngf // 16)
 
     def forward(self, z_code, c_code=None, p_code=None, cat_code=None):
+        print(c_code, z_code)
+        print(cfg.GAN.B_CONDITION)
+        print(cfg.GAN.P_CONDITION)
         if cfg.GAN.B_CONDITION and c_code is not None:
             in_code = torch.cat((c_code, z_code), 1)
         elif cfg.GAN.B_CONDITION and c_code is not None and cfg.GAN.P_CONDITION and p_code is not None:
