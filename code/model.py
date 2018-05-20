0e11bae6728c57dc0991501f37b4b66cd8b64fc4
tboquet <thomas.boquet@hec.ca>
2018-05-20 16:22:42 -0400

    * forward generator debug

--- code/model.py
+++ code/model.py
@@ -192,10 +192,6 @@ class INIT_STAGE_G(nn.Module):
         self.upsample4 = upBlock(ngf // 8, ngf // 16)
 
     def forward(self, z_code, c_code=None, p_code=None, cat_code=None):
-        # print('c_code: {}, z_code: {}'.format(c_code.size(), z_code.size()))
-        # print('p_code: {}'.format(p_code.size()))
-        # print(cfg.GAN.B_CONDITION)
-        # print(cfg.GAN.P_CONDITION)
         if cfg.GAN.B_CONDITION and c_code is not None and cfg.GAN.P_CONDITION is False:
             in_code = torch.cat((c_code, z_code), 1)
         elif cfg.GAN.B_CONDITION and c_code is not None and cfg.GAN.P_CONDITION and p_code is not None:
@@ -203,7 +199,6 @@ class INIT_STAGE_G(nn.Module):
         else:
             in_code = z_code
 
-        # print(in_code)
         # state size 16ngf x 4 x 4
         out_code = self.fc(in_code)
         out_code = out_code.view(-1, self.gf_dim, 4, 4)
@@ -312,6 +307,13 @@ class G_NET(nn.Module):
         else:
             p_code, mu, logvar = z_code, None, None
         fake_imgs = []
+        print(self.h_net1)
+        print(self.h_net2)
+        print(self.h_net3)
+        print('IMAGES')
+        print(self.img_net1)
+        print(self.img_net2)
+        print(self.img_net3)
         if cfg.TREE.BRANCH_NUM > 0:
             h_code1 = self.h_net1(z_code, c_code, p_code)
             fake_img1 = self.img_net1(h_code1)
